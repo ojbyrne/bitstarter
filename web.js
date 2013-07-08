b5894417e7ecd0ca513444a8209c40f42e918c6d
Owen Byrne <ojbyrne@gmail.com>
2013-07-08 16:57:36 -0700

    Reading from index.html

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+var fs = require('fs');
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var indexBuffer = fs.readFileSync("index.html");
+  response.send(indexBuffer.toString());
 });
 
 var port = process.env.PORT || 5000;
